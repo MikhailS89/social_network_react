7449104e8e064a4c084365314f353f3271317c8c
MikhailS89 <mihey2023@ya.ru>
2021-11-13 20:10:12 +0400

    create homework lesson 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
-import './App.scss';
-import Message from './Message/Message';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import Message from "./Message/Message";
 
-const learn = 'Message';
+export default function App() {
+  const [messageList, sendMessageList] = useState([]);
+  const [value, setValue] = useState('');
+  const [author, setAuthor] = useState('');
+  
+  const sendRobot = (messageList) => {
+    if(messageList.length){
+      if(messageList[messageList.length - 1].author !== 'Robot'){
+        setTimeout(() => {
+          sendMessageList([
+            ...messageList,
+            {text: `messega send ${messageList[messageList.length - 1].author}`, author: 'Robot', id: Date.now() },
+          ])
+        }, 1500)
+      }
+    }
+  }
 
-function App() {
+  useEffect(() => {
+    sendRobot(messageList)
+  }, [messageList])
+
+
+  const changeText = (event) => {
+    setValue(event.target.value);
+  };
+  const changeAuthor = (event) => {
+    setAuthor(event.target.value);
+  };
+  const sendMessage = (event) => {
+    event.preventDefault();
+    sendMessageList([
+      ...messageList,
+      { text: value, author: author, id: Date.now() },
+    ])
+    setValue('')
+    setAuthor('')
+  };
+
   return (
     <div className="app">
       <header className="app__header">
         <div className="app__wrp">
-          <Message title={learn}/>
+          {messageList.map((message) => (
+            <Message
+              key={message.id}
+              text={message.text}
+              author={message.author}
+            />
+          ))}
         </div>
       </header>
+      <main className="main">
+        <form className="main__form">
+          <textarea value={value} onChange={changeText} className="main__message"></textarea>
+          <div className="main__wrp">
+            <input value={author} onInput={changeAuthor} className="main__author"></input>
+            <button onClick={sendMessage} className="main__send">
+              send
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
 
-export default App;
